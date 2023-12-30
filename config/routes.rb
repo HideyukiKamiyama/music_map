2e93f5c05b47644bbb402e5545c9743df2adb50a
HideyukiKamiyama <happy.magical0722@gmial.com>
2023-12-30 23:36:38 +0900

    add: アバター画像を除くプロフィール詳細画面を作成しました

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root "static_pages#top"
   resources :users, only: %i[new create]
 
+  resource :profile,only: %i[show edit update]
+
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
