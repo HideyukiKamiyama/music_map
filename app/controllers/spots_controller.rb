a1d7848a0013df59d201d9f69fb716ef80b4b10e
HideyukiKamiyama <happy.magical0722@gmial.com>
2024-01-03 17:02:38 +0900

    add: Spotify APIを導入し投稿時にアーティスト名のチェックを行えるようにしました

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,8 +1,9 @@
 class SpotsController < ApplicationController
   skip_before_action :require_login, only: %i[index]
+  before_action :check_artist_name, only: %i[create]
 
   def index
-    @spots = Spot.all
+    @spots = Spot.all.order(updated_at: "DESC")
   end
 
   def show; end
@@ -14,9 +15,13 @@ class SpotsController < ApplicationController
   # def edit; end
 
   def create
-    @spot = ArtistSpot.new(spot_params.merge(user_id: current_user.id))
-    if @spot.save
-      redirect_to spots_path
+    if check_artist_name
+      @spot = ArtistSpot.new(spot_params.merge(user_id: current_user.id))
+      if @spot.save
+        redirect_to spots_path
+      else
+        render :new
+      end
     else
       render :new
     end
@@ -34,5 +39,13 @@ class SpotsController < ApplicationController
 
   def spot_params
     params.require(:artist_spot).permit(:tag, :spot_name, :name, :detail)
+  end
+
+  # Spotify API上のデータと比較することで正確なアーティスト名が入力されているかチェックするメソッド
+  def check_artist_name
+    artist_name = params["artist_spot"]["name"]
+    spotify_data = RSpotify::Artist.search(artist_name).first
+    spotify_name = spotify_data.name
+    artist_name == spotify_name
   end
 end
