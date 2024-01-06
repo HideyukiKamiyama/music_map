b38975316c9634c776d6a791f3cce302cf9925f7
HideyukiKamiyama <happy.magical0722@gmial.com>
2024-01-06 22:52:34 +0900

    add: ブックマーク機能のajax化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,11 @@
 class BookmarksController < ApplicationController
   def create
-    spot = Spot.find(params[:spot_id])
-    current_user.bookmark(spot)
-    redirect_to spots_path
+    @spot = Spot.find(params[:spot_id])
+    current_user.bookmark(@spot)
   end
 
   def destroy
-    spot = current_user.bookmarks.find(params[:id]).spot
-    current_user.unbookmark(spot)
-    redirect_back fallback_location: spots_path, status: :see_other
+    @spot = current_user.bookmarks.find(params[:id]).spot
+    current_user.unbookmark(@spot)
   end
 end
