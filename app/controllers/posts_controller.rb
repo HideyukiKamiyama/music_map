518cca2bf27a97a0628ecbf856603f63e33a5b3c
HideyukiKamiyama <happy.magical0722@gmial.com>
2024-01-07 16:17:20 +0900

    add: 投稿編集機能を実装しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,19 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show]
+  before_action :set_post, only: %i[edit update]
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 
   def new
     @post = Post.new
     @spot = Spot.find(params[:spot_id])
   end
 
+  def edit; end
+
   def create
-    @post = current_user.posts.build(post_params)
+    @post = current_user.posts.build(post_params.merge(spot_id: params[:spot_id]))
     if @post.save
       redirect_to spot_path(@post.spot_id)
     else
@@ -17,13 +21,21 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :body).merge(spot_id: params[:spot_id])
+    params.require(:post).permit(:title, :body)
   end
 
   def set_post
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 end
