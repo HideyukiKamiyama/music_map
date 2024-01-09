9b3caf706d84ec2b57234c52c409ac8ed01064f0
HideyukiKamiyama <happy.magical0722@gmial.com>
2024-01-09 22:14:46 +0900

    update: 会員登録成功時にログイン状態になるよう変更しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "会員登録が成功しました"
+      auto_login(@user)
+      redirect_to spots_path, notice: "会員登録が成功しました"
     else
       flash.now[:alert] = "会員登録に失敗しました"
       render :new, status: :unprocessable_entity
