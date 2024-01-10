deeac40cf67fb1ff91d1059ee77bcf0d2551f6f1
HideyukiKamiyama <happy.magical0722@gmial.com>
2024-01-10 22:55:10 +0900

    fix: spotsテーブルのaddressカラムにnull制約を追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_08_084449) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_134640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_084449) do
     t.bigint "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "address"
+    t.string "address", null: false
     t.float "latitude"
     t.float "longitude"
     t.index ["artist_id"], name: "index_spots_on_artist_id"
