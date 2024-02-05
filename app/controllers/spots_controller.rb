5cf94ca609d9bad29a5f3569df883eb9f08e0200
HideyukiKamiyama <happy.magical0722@gmial.com>
2024-02-05 22:37:43 +0900

    add: 聖地削除機能

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -37,7 +37,7 @@ class SpotsController < ApplicationController
   def create
     @artist_spot = ArtistSpot.new(spot_params.merge(user_id: current_user.id))
     if @artist_spot.save
-      redirect_to spots_path, data: { turbo: false }, notice: t('.notice')
+      redirect_to spots_path, notice: t('.notice')
     else
       flash.now[:alert] = t('.alert')
       render :new, status: :unprocessable_entity
@@ -47,11 +47,17 @@ class SpotsController < ApplicationController
   def update
     @artist_spot = ArtistSpot.new(spot_params.merge(user_id: current_user.id, id: params[:id]))
     if @artist_spot.save
-      redirect_to spot_path(@artist_spot.id), data: { turbo: false }, notice: t('.notice')
+      redirect_to spot_path(@artist_spot.id), notice: t('.notice')
     else
       flash.now[:alert] = t('.alert')
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    spot = current_user.spots.find(params[:id])
+    spot.destroy!
+    redirect_to spots_path, notice: t('.notice'), status: :see_other
   end
 
   def bookmarks
